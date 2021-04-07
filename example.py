648ff38332d798b3703e87801550cd6a97e0e9df
sueskind <52210599+sueskind@users.noreply.github.com>
2021-04-07 18:21:59 +0200

    Add example

---
+++ example.py
@@ -0,0 +1,6 @@
+from src.telegram import Notifier
+
+if __name__ == '__main__':
+    n = Notifier(bot_token="1234567890:ABCDEFGHIJKLMNOPQRSTUVWXYZ", country="DE")
+    n.update_offers()
+    n.notify(chat_id=123456)
