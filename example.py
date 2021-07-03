74555a204fdcff36e4ed7e26368fbde612b7b7f1
sueskind <52210599+sueskind@users.noreply.github.com>
2021-07-03 17:23:53 +0200

    Add signal example

--- example.py
+++ example.py
@@ -4,3 +4,7 @@ if __name__ == '__main__':
     n = Notifier(service=Notifier.TELEGRAM, telegram_bot_token="1234567890:ABCDEFGHIJKLMNOPQRSTUVWXYZ", country="DE")
     n.update_offers()
     n.notify(recipients=[123456])
+
+    n = Notifier(service=Notifier.SIGNAL, signal_sender_number="+123456789", country="DE")
+    n.update_offers()
+    n.notify(recipients=["+123987654"])
